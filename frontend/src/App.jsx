9ef7d2e983c8355cb1a8fec30c96356e4db66fe5
mohnishgorana1 <mohnishgorana1@gmail.com>
2023-12-15 23:48:31 +0530

    add routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,27 @@
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Profile from "./pages/Profile";
+import Header from "./components/Header";
 
 function App() {
-
   return (
-    <div className=""></div>
-  )
+    <BrowserRouter>
+      {/* header */}
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+
+        {/* //TODO : add Profile page to private route */}
+        <Route path='/profile' element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
